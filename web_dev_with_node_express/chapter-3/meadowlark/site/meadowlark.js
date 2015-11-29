86382b9d6c701aab79c610b21168b0ffc92c37b1
volving <volving@icloud.com>
2015-11-30 00:00:23 +0800

    Debug: JS: correct misused 'use' function on express [app]

--- web_dev_with_node_express/chapter-3/meadowlark/site/meadowlark.js
+++ web_dev_with_node_express/chapter-3/meadowlark/site/meadowlark.js
@@ -1,36 +1,32 @@
 var express = require('express');
 
 var app = express();
+
 //B-1: Install express-handlebars view engine //
 var hbs = require('express-handlebars').create({defualtLayout: 'main', extname: '.hbs'});
 app.engine('hbs', hbs.engine);
-app.use('view engine', 'hbs');
+app.set('view engine', 'hbs');
+
 //B-1: end //
 
 app.set('port', process.env.PORT || 8000);
 
 app.get('/', function(req, res){
-	res.type('text/plain');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
 
 app.get('/about', function(req, res){
-	res.type('text/plain');
-	res.send('About Meadowlark Travel');
+	res.render('about');
 });
 
-
 app.use(function(req, res){
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not Found');
+	res.status('404');
+	res.render('404');
 });
 
 app.use(function(err, req, res){
-	console.log(err.stack);
-	res.type('text/plain');
-	res.status(500);
-	res.send('500 - Server Error');
+	res.status('500');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
