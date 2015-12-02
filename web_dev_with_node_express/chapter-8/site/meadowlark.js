b75419039ed3389f413ac106d46dc72b561605e6
volving <volving@icloud.com>
2015-12-03 01:23:42 +0800

    Add: Ajax-Form: trigger ajax forms

--- web_dev_with_node_express/chapter-8/site/meadowlark.js
+++ web_dev_with_node_express/chapter-8/site/meadowlark.js
@@ -16,32 +16,44 @@ app.set('view engine', 'hbs');
 app.use(bodyparser());
 
 //注意两个字串在拼接时加上路径斜杠／
-app.use(express.static( __dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 // app.use('/vendors', express.static(__dirname + '/public/vendors'));
 
 
 app.set('port', process.env.PORT || 8000);
 
-app.get('/', function(req, res){
+
+app.get('/', function(req, res) {
     res.render('home', {
         quote: 'The quick fox jumps over the lazy dog!'
     });
 });
 
 
-app.get('/newsletter', function(req, res){
-    res.render('newsletters', {csrf:'a little secret thing happened'});
+app.get('/process', function(req, res) {
+    res.render('ajax_newsletter', {
+        csrf: 'a little secret thing happened'
+    });
 });
-app.post('/process', function(req, res){
-    console.log(req.body._csrf);
-    console.log(req.body.name);
-    console.log(req.body.email);
-    console.log(typeof req.query);
-    console.log(req.query);
-    res.redirect('/', '303');
+
+app.post('/process', function(req, res) {
+    if (req.xhr || req.accepts('json,html') === 'json') {
+        res.send({success: true});
+    } else {
+        // console.log(req.body._csrf);
+        // console.log(req.body.name);
+        // console.log(req.body.email);
+        // console.log(typeof req.query);
+        // console.log(req.query);
+        res.redirect('/', '303');
+    }
 });
-app.get('/', function(req, res){
-    res.render('home', {quote: 'Have a seat!'});
+
+
+app.get('/', function(req, res) {
+    res.render('home', {
+        quote: 'Have a seat!'
+    });
 });
 app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost' + app.get('port') + '; press Ctrl+c to terminate.');
